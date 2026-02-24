e8bbf4e3f1bcf4483bb630ca574bab9b58265c12
Uy Ha <hchanuy@gmail.com>
2026-02-24 21:52:50 +0100

    finished parsing resources

--- src/Ast.zig
+++ src/Ast.zig
@@ -1,10 +1,17 @@
 const Ast = @This();
 pub const Error = std.mem.Allocator.Error;
 
-root: usize,
-definitions: std.ArrayList(Definition),
+nodes: std.ArrayList(Node),
 
-pub const Definition = union(enum) {
+pub fn root(self: *const Ast) usize {
+    return self.nodes.items.len - 1;
+}
+
+pub fn iterator(self: *const Ast, gpa: Allocator) Allocator.Error!Iterator {
+    return try .init(gpa, self.root(), self.nodes.items);
+}
+
+pub const Node = union(enum) {
     root: Root,
 
     resources: Resources,
@@ -29,7 +36,7 @@ pub const Definition = union(enum) {
 pub const Events = std.ArrayList(usize);
 
 pub const Root = struct {
-    name: usize,
+    name: ?usize,
     brace_left: usize,
     brace_right: usize,
 
@@ -60,28 +67,29 @@ pub fn parse(
     content: []const u8,
     tokens: []const Token,
 ) Error!Ast {
-    var definitions: std.ArrayList(Definition) = .empty;
+    var ast: Ast = .{ .nodes = .empty };
     var state: State = .{
         .arena = arena,
 
-        .definitions = &definitions,
+        .nodes = &ast.nodes,
         .content = content,
         .tokens = tokens,
     };
-    return .{ .root = try state.parse(), .definitions = definitions };
+    try state.parse();
+    return ast;
 }
 
 const State = struct {
     arena: Allocator,
 
-    definitions: *std.ArrayList(Definition),
+    nodes: *std.ArrayList(Node),
     content: []const u8,
     tokens: []const Token,
 
     current: usize = 0,
 
-    fn parse(self: *State) Error!usize {
-        return self.root();
+    fn parse(self: *State) Error!void {
+        _ = try self.root();
     }
 
     fn currentToken(self: *const State) Token {
@@ -92,9 +100,9 @@ const State = struct {
         assert(self.current < self.tokens.len);
         return source.lexeme(self.content);
     }
-    fn append(self: *State, value: Definition) Error!usize {
-        try self.definitions.append(self.arena, value);
-        return self.definitions.items.len - 1;
+    fn append(self: *State, value: Node) Error!usize {
+        try self.nodes.append(self.arena, value);
+        return self.nodes.items.len - 1;
     }
     fn unexpected(self: *State) Error!usize {
         defer self.current += 1;
@@ -105,6 +113,9 @@ const State = struct {
     }
     fn named(self: *State, comptime expected: []const u8) Error!?usize {
         const current = self.currentToken();
+        if (current.type == .eof) {
+            return try self.append(@unionInit(Node, "missing", .identifier));
+        }
         if (current.type != .identifier or
             !std.mem.eql(u8, expected, self.lexeme(current)))
         {
@@ -116,12 +127,24 @@ const State = struct {
     }
     fn token(self: *State, comptime expected: Token.Type) Error!?usize {
         const current = self.currentToken();
+        if (current.type == .eof) {
+            return try self.append(@unionInit(Node, "missing", expected));
+        }
         if (current.type != expected) {
             return null;
         }
 
         self.current += 1;
-        return try self.append(@unionInit(Definition, @tagName(expected), current));
+        return try self.append(@unionInit(Node, @tagName(expected), current));
+    }
+    fn tokenTrailing(self: *State, comptime expected: Token.Type) Error!?usize {
+        const current = self.currentToken();
+        if (current.type != expected) {
+            return null;
+        }
+
+        self.current += 1;
+        return try self.append(@unionInit(Node, @tagName(expected), current));
     }
     fn tokenOrMissing(self: *State, comptime expected: Token.Type) Error!usize {
         if (try self.token(expected)) |index| {
@@ -131,7 +154,7 @@ const State = struct {
     }
 
     fn root(self: *State) Error!usize {
-        const name_index = try self.token(.identifier) orelse try self.unexpected();
+        const name_index = try self.token(.identifier);
 
         var components: std.ArrayList(usize) = .empty;
         var commas: std.ArrayList(usize) = .empty;
@@ -152,9 +175,9 @@ const State = struct {
                 if (try self.component()) |index| {
                     try components.append(self.arena, index);
                 } else {
-                    break :blk try self.unexpected();
+                    break :blk try self.missing(.identifier);
                 }
-                if (try self.token(.comma)) |index| {
+                if (try self.tokenTrailing(.comma)) |index| {
                     try commas.append(self.arena, index);
                 }
 
@@ -232,6 +255,85 @@ const State = struct {
             .@"volatile" = try self.token(.@"volatile"),
             .type = try self.tokenOrMissing(.identifier),
         } });
+    }
+};
+
+pub const Iterator = struct {
+    stack: std.ArrayList(usize) = .empty,
+    nodes: []const Node,
+
+    pub fn init(gpa: Allocator, ast_root: usize, nodes: []const Node) Allocator.Error!Iterator {
+        var result: Iterator = .{ .nodes = nodes };
+
+        try result.stack.append(gpa, ast_root);
+
+        return result;
+    }
+
+    pub fn next(self: *Iterator, gpa: Allocator) Allocator.Error!?Node {
+        const node = self.nodes[self.stack.pop() orelse return null];
+
+        switch (node) {
+            .root => |payload| {
+                try self.stack.append(gpa, payload.brace_right);
+                try self.appendItemsCommas(
+                    gpa,
+                    payload.items.items,
+                    payload.commas.items,
+                );
+                try self.stack.append(gpa, payload.brace_left);
+                if (payload.name) |name| {
+                    try self.stack.append(gpa, name);
+                }
+            },
+            .resources => |payload| {
+                try self.stack.append(gpa, payload.brace_right);
+                try self.appendItemsCommas(
+                    gpa,
+                    payload.items.items,
+                    payload.commas.items,
+                );
+                try self.stack.append(gpa, payload.brace_left);
+                try self.stack.append(gpa, payload.name);
+            },
+            .resource => |payload| {
+                try self.stack.append(gpa, payload.type);
+                if (payload.@"volatile") |i| {
+                    try self.stack.append(gpa, i);
+                }
+                if (payload.@"const") |i| {
+                    try self.stack.append(gpa, i);
+                }
+                if (payload.star) |i| {
+                    try self.stack.append(gpa, i);
+                }
+                try self.stack.append(gpa, payload.colon);
+                try self.stack.append(gpa, payload.name);
+            },
+            else => {},
+        }
+
+        return node;
+    }
+
+    fn appendItemsCommas(
+        self: *Iterator,
+        gpa: Allocator,
+        items: []const usize,
+        commas: []const usize,
+    ) Allocator.Error!void {
+        assert(items.len == commas.len or items.len == commas.len + 1);
+
+        if (items.len == commas.len + 1) {
+            try self.stack.append(gpa, items[items.len - 1]);
+        }
+
+        var bound = commas.len;
+        while (bound > 0) : (bound -= 1) {
+            const i = bound - 1;
+            try self.stack.append(gpa, commas[i]);
+            try self.stack.append(gpa, items[i]);
+        }
     }
 };
 
